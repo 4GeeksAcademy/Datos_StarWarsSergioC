b5097f845653e6a88965d492176e7a4b60a68c9d
Sergio Cordoba <sergiocordoba246@gmail.com>
2026-01-12 19:59:38 +0000

    A la espera de Facundo

--- src/models.py
+++ src/models.py
@@ -6,14 +6,46 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    password: Mapped[str] = mapped_column(nullable=False)    
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,            
         }
+    
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+       
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.name,            
+        }
+
+class Characters(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    original_planet: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+       
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.name,            
+        }
+    
+class Starships(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+       
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.name,            
+        }    
